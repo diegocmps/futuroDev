d2f72dc0bc90103992ffaff36176efba271cf070
diegocmps <diegobarreiros@hotmail.com>
2024-07-14 17:27:58 -0300

    semana 07 - exercicio 01

---
+++ modulo_02/exercicios_semana_07/exercicio_semana_07/src/App.jsx
@@ -0,0 +1,21 @@
+
+import { Contador } from './components/Contador'
+function App() {
+
+
+  return (
+
+    <div>
+
+      <div className='list-contador'>
+      <Contador />
+
+
+      </div>        
+
+    </div>
+
+  )
+}
+
+export default App
---
+++ modulo_02/exercicios_semana_07/exercicio_semana_07/src/components/Contador.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export function Contador() {
+    const [contador, setContador] = useState(0)
+
+
+    function handleAdd() {
+        setContador((estadoAnterior) => {
+            return estadoAnterior + 1
+        })
+    }
+
+    function handleRemove() {
+        setContador((estadoAnterior) => {
+            if(estadoAnterior - 1 < 0) return 0
+            return estadoAnterior - 1
+
+        })
+    }
+
+    return (
+        <div className="contador">
+
+            <button onClick={handleRemove}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"  height={16} width={16}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+                </svg>
+
+            </button>
+
+
+            <p>
+                Valor: {' '}
+                <strong>{contador}</strong>
+            </p>
+
+            <button onClick={handleAdd}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" height={16} width={16}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+
+            </button>
+        </div>
+    )
+}
